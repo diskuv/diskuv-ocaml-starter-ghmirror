dab08e7d343b6d7fb71d8b119455941077b33d06
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2021-08-23 18:15:56 -0700

    Use Real World OCaml example

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,17 @@
-let () = print_endline "Hello, World!"
+(* The "Complete Program" code example from Real World OCaml.
+
+  Source and explanation: https://dev.realworldocaml.org/guided-tour.html
+  UNLICENSE licensed for code examples: https://github.com/realworldocaml/book/blob/0f3e9708174115d133ecd6993a6070288f9eb221/LICENSE.md
+ *)
+
+open Base
+open Stdio
+
+let rec read_and_accumulate accum =
+  let line = In_channel.input_line In_channel.stdin in
+  match line with
+  | None -> accum
+  | Some x -> read_and_accumulate (accum +. Float.of_string x)
+
+let () =
+  printf "Total: %F\n" (read_and_accumulate 0.)
