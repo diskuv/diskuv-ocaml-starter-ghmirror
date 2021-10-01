ab87d148842eeb2f9a72abf7aee07f58eb0825f5
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2021-10-01 16:16:57 -0700

    Use troubleshooting hook

--- .github/workflows/troubleshoot-example-ppx_sexp_conv.sh
+++ .github/workflows/troubleshoot-example-ppx_sexp_conv.sh
@@ -5,21 +5,26 @@
 #
 set -euf
 
-echo 'Starting buildconfig/troubleshooting-ppx_sexp_conv.sh' >&2
+echo 'Starting troubleshooting-example-ppx_sexp_conv.sh' >&2
 set -x
 
 # Basic diagnostics
 pwd
+which dune
+which opam
 opam env
 
-# Use the system switch since as a pre-existing switch that has an installed OCaml compiler base package.
-# shellcheck disable=SC2046
-eval $(opam env --switch "$LOCALAPPDATA\\Programs\\DiskuvOCaml\\0\\system" --set-switch)
+# When called as a troubleshooting hook, then OPAMSWITCH will already be set. Otherwise we are being
+# called directly in a CI build.
+if [ -z "${OPAMSWITCH:-}" ]; then
+    # We are being called directly in a CI build.
+    # Use the system switch as a pre-existing switch that has an installed OCaml compiler base package.
+    # shellcheck disable=SC2046
+    eval $(opam env --switch "$LOCALAPPDATA\\Programs\\DiskuvOCaml\\0\\system" --set-switch)
+fi
 opam switch
 
 # Use a clean directory
-export TOPDIR
-TOPDIR="$(pwd)"
 install -d _ci/troubleshoot
 cd _ci/troubleshoot
 
@@ -31,6 +36,6 @@ set +f # Turn on file globbing so we can do wildcards ('*').
 
 opam source ppx_sexp_conv
 cd ppx_sexp_conv.*
-tail -v -n5000 ./*opam                             # Pretty print all the Opam packages
-opam install ./*opam --deps-only --with-test --yes # Install all the dependencies of the packages
-dune build -p ppx_sexp_conv -j1 --verbose          # This is the first statement in ppx_sexp_conv.opam build:[] block, except we want verbose to see what is happening
+tail -v -n5000 ./*opam                    # Pretty print all the Opam packages
+opam install ./*opam --deps-only --yes    # Install all the dependencies of the packages
+dune build -p ppx_sexp_conv -j1 --verbose # This is the first statement in ppx_sexp_conv.opam build:[] block, except we want verbose to see what is happening
