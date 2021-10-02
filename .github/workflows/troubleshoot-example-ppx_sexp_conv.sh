1247742bdbaee0db22ee2e9dac6e8c3365a6bfd7
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2021-10-01 23:39:54 -0700

    Add compiler to troubleshooting

--- .github/workflows/troubleshoot-example-ppx_sexp_conv.sh
+++ .github/workflows/troubleshoot-example-ppx_sexp_conv.sh
@@ -6,13 +6,21 @@
 set -euf
 
 echo 'Starting troubleshooting-example-ppx_sexp_conv.sh' >&2
-set -x
 
 # Basic diagnostics
 pwd
 which dune
 which opam
 opam env
+
+# Pull in utilities for getting compiler
+# shellcheck disable=SC1091
+. vendor/diskuv-ocaml/etc/contexts/linux-build/crossplatform-functions.sh
+
+# Ensure a clean directory (in case this script is called repeatedly)
+install -d _ci/troubleshoot
+rm -rf _ci/troubleshoot
+install -d _ci/troubleshoot
 
 # Remove trailing slashes, if any, from PATH which can be expanded to include double quotes that
 # interfere with Microsoft's vsdevcmd.bat
@@ -31,9 +39,9 @@ elif [ -z "${OPAMSWITCH:-}" ]; then
 fi
 opam switch
 
-# Use a clean directory
-install -d _ci/troubleshoot
-cd _ci/troubleshoot
+# More diagnostics
+which dune
+opam var
 
 set +f # Turn on file globbing so we can do wildcards ('*').
 
@@ -41,8 +49,11 @@ set +f # Turn on file globbing so we can do wildcards ('*').
 # Real troubleshooting
 # --------------------
 
-opam source ppx_sexp_conv
+set -x
+cd _ci/troubleshoot
+autodetect_compiler launch-compiler.sh                          # Create a script to launch a program within an environment containing the compiler (MSVC, etc.)
+opam source ppx_sexp_conv                                       # Get the source code for package; version from the Opam switch (ex. pinned package)
 cd ppx_sexp_conv.*
-tail -v -n5000 ./*opam                    # Pretty print all the Opam packages
-opam install ./*opam --deps-only --yes    # Install all the dependencies of the packages
-dune build -p ppx_sexp_conv -j1 --verbose # This is the first statement in ppx_sexp_conv.opam build:[] block, except we want verbose to see what is happening
+tail -v -n5000 ./*opam                                          # Pretty print all the Opam packages
+opam install ./*opam --deps-only --yes                          # Install all the dependencies of the packages
+../launch-compiler.sh dune build -p ppx_sexp_conv -j1 --verbose # This is the first statement in ppx_sexp_conv.opam build:[] block, except we want verbose to see what is happening
