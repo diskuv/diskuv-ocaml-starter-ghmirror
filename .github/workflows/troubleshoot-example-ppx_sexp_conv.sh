df0a26b5faa15fff002e739f64c3bb44720e539d
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2021-10-02 17:57:44 -0700

    Set OPAMCLI inside troubleshooting

--- .github/workflows/troubleshoot-example-ppx_sexp_conv.sh
+++ .github/workflows/troubleshoot-example-ppx_sexp_conv.sh
@@ -50,7 +50,8 @@ set +f # Turn on file globbing so we can do wildcards ('*').
 # --------------------
 
 set -x
-export MSYS2_ARG_CONV_EXCL='*'                                  # Mimic setting inside `opam install`
+export OPAMCLI=2.0                                              # Mimic environment inside `opam install`
+export MSYS2_ARG_CONV_EXCL='*'                                  # Mimic environment inside `opam install`
 cd _ci/troubleshoot
 autodetect_compiler launch-compiler.sh                          # Create a script to launch a program within an environment containing the compiler (MSVC, etc.)
 opam source ppx_sexp_conv                                       # Get the source code for package; version from the Opam switch (ex. pinned package)
