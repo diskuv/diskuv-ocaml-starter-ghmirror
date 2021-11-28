3c49e874b6f328c74250ea8f45f45acf7e1c0ef4
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2021-11-27 23:08:25 -0800

    Remove excess backslash in makeit.cmd

--- makeit.cmd
+++ makeit.cmd
@@ -4,4 +4,4 @@ REM ---------------------------------------------
 REM Command file for running Makefile using MSYS2
 REM ---------------------------------------------
 REM
-CALL %~dp0\vendor\diskuv-ocaml\runtime\windows\makeit.cmd %*
+CALL %~dp0vendor\diskuv-ocaml\runtime\windows\makeit.cmd %*
